1a09812bc5cc6bd1d15fcee7e8a58267beb85f51
Yonghang Wu <wuyonghang12138@gmail.com>
2022-04-13 16:26:30 +0800

    适配FireFox

--- script.js
+++ script.js
@@ -1,37 +1,72 @@
-const videoElement = document.querySelector('video');
-const button = document.querySelector('#start');
-let mediaStream = null;
+const videoElement = document.querySelector("video");
+const buttonStart = document.querySelector("#start");
+const buttonSelect = document.querySelector("#chose");
+let isSelected = false;
 
-let isCaptured = false;
+buttonSelect.addEventListener("click", function () {
+  selectMediaStream()
+    .then((finished) => {
+      if (finished) {
+        videoElement.play();
 
-selectMediaStream();
-
-button.addEventListener('click', async function () {
-  // close picture in picture and sharing if it is captured
-  if (isCaptured) {
-    document.exitPictureInPicture();
-    // mediaStream.getTracks().forEach(track => track.stop());
-    button.textContent="START";
-    isCaptured = false;
-  // select a screen and show it in Picture-In-Picture
-  } else {
-    await videoElement.requestPictureInPicture();
-    isCaptured = true;
-    button.textContent = "STOP";
-  }
+        // Check if picture-in-picture mode is available
+        if (document.pictureInPictureEnabled) {
+          buttonSelect.parentNode.hidden = true;
+          buttonStart.parentNode.hidden = false;
+          buttonStart.addEventListener("click", enterPictureInPicture);
+        } else {
+          buttonSelect.parentNode.hidden = true;
+          videoElement.hidden = false;
+        }
+      }
+    })
+    .catch((error) =>
+      console.log("An error occurs in selectMediaStream --- " + error)
+    );
 });
 
-// Let user chose an screen to capture
-async function selectMediaStream() {
-  try {
-    mediaStream = await navigator.mediaDevices.getDisplayMedia();
-    videoElement.srcObject = mediaStream;
-    // video is loaded
-    videoElement.onloadedmetadata = () => {
-      videoElement.play();
+/*
+  =================================
+  Enter the Picture-In-Picture Mode
+  =================================
+*/
+async function enterPictureInPicture() {
+  // close picture in picture and sharing if it is captured
+  if (isSelected) {
+    document.exitPictureInPicture();
+    buttonStart.textContent = "START";
+    isSelected = false;
+    // select a screen and show it in Picture-In-Picture
+  } else {
+    try {
+      await videoElement.requestPictureInPicture();
+      isSelected = true;
+      buttonStart.textContent = "STOP";
+    } catch (error) {
+      console.log("Requet Picture In Picture Error: " + error);
     }
-    return "selected";
-  } catch (error) {
-    console.log(error);
   }
+}
+
+/*
+  ===========================================
+  Let user select an screen to capture,
+  return a promise after the screen is loaded
+  ===========================================
+*/
+function selectMediaStream() {
+  buttonSelect.disabled = true;
+  let mediaStreamPromise = navigator.mediaDevices.getDisplayMedia({
+    audio: true,
+    video: true,
+  });
+  return mediaStreamPromise
+    .then((mediaStream) => (videoElement.srcObject = mediaStream))
+    .then(
+      () =>
+        new Promise((resovle) => {
+          videoElement.onloadedmetadata = () => resovle(true);
+        })
+    )
+    .catch((error) => console.log("getDisplayMedia Error: " + error));
 }
